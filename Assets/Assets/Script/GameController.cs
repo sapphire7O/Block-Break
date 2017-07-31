f1bfc607d6cc80719163cb5db76f3fad24d55267
sapphire7O <pieater70@gmail.com>
2017-07-31 14:07:38 -0700

    Update camera in game to only support 16:9 resolution and fixed UI / Walls to the new carmera size.

---
+++ Assets/Assets/Script/GameController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour 
+{
+
+	int lives = 3;
+	int score = 0;
+
+	public Text scoreText;
+	public Text livesText;
+
+	public int Lives
+	{
+		get { return lives; }
+		set
+		{
+			lives = value;
+			UpdateUI ();
+		}
+	}
+
+	public int Score
+	{
+		get { return score; }
+		set 
+		{
+			score = value;
+			UpdateUI();
+		}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		UpdateUI();
+	}
+
+	void UpdateUI()
+	{
+		scoreText.text = string.Format ("Score: {0}", score);
+		livesText.text = string.Format ("Lives: {0}", lives);
+	}
+}
---
+++ Assets/Assets/Script/destroyblock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyblock : MonoBehaviour {
+
+	public Transform boomObj;
+	public Transform powerUpSpeedObj;
+	public Transform powerUpSizeObj;
+
+	public int whichpowerup;
+
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		whichpowerup = Random.Range (1, 15);
+
+		if (whichpowerup == 1) 
+		{ 
+			Instantiate (powerUpSizeObj, transform.position, powerUpSizeObj.rotation);
+		}
+
+		if (whichpowerup == 2) 
+		{ 
+			Instantiate (powerUpSpeedObj, transform.position, powerUpSpeedObj.rotation);
+		}
+
+		Destroy (gameObject, .05f);
+		Instantiate (boomObj, transform.position, boomObj.rotation);
+	}
+}
